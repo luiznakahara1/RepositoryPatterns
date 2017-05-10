88c65afbed391b01284c6e80025a50e2ae27ffe7
luiz toshimitsu nakahara junior <luiz.nakahara@benner.com.br>
2017-05-10 16:19:58 -0300

    primeiro commit

---
+++ RepositoryPatterns/RepositoryPatterns/Padrao/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatterns.Padrao
+{
+    public interface IRepositorio <T>
+    {
+        T GetById(int id);
+        void Salvar(T entidade);
+        void Deletar(T entidade);
+    }
+}
